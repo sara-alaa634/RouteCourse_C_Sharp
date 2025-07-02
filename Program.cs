f936cce416a2d38bb32312824b2758e5a5d121a2
sara alaa <zaa3748@gmail.com>
2025-07-03 01:30:10 +0300

    assigment1

--- Program.cs
+++ Program.cs
@@ -306,30 +306,30 @@ namespace Assigment_S3
                 Output: Days in Month: 31
              */
 
-            Console.Write("enter month number: ");
-            int.TryParse(Console.ReadLine(), out int month);
+            //Console.Write("enter month number: ");
+            //int.TryParse(Console.ReadLine(), out int month);
 
-            switch (month) { 
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                Console.WriteLine("days in this month is :31");
-                break;
-            case 2:
-                Console.WriteLine("days in this month is :28");
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                Console.WriteLine("days in this month is :30");
-                break;
+            //switch (month) { 
+            //case 1:
+            //case 3:
+            //case 5:
+            //case 7:
+            //case 8:
+            //case 10:
+            //case 12:
+            //    Console.WriteLine("days in this month is :31");
+            //    break;
+            //case 2:
+            //    Console.WriteLine("days in this month is :28");
+            //    break;
+            //case 4:
+            //case 6:
+            //case 9:
+            //case 11:
+            //    Console.WriteLine("days in this month is :30");
+            //    break;
 
-            }
+            //}
             #endregion
 
 
